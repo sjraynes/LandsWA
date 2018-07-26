b3c09a87f7fa8abf54a8f9cd56c4783c6f88b70a
Ravi Yadav <ravs_avs@yahoo.com>
2018-07-26 08:53:59 +0800

    Added code to perform Applicant Search

--- LandsWa.Acceptance.Smoke.Tests/Tests/LoginPageTests.cs
+++ LandsWa.Acceptance.Smoke.Tests/Tests/LoginPageTests.cs
@@ -1,17 +1,39 @@
 ﻿using LandsWa.Acceptance.Smoke.Tests.Pages;
 using LandsWa.Acceptance.Smoke.Tests.SetupTeardown;
 using NUnit.Framework;
+using static LandsWa.Acceptance.Smoke.Tests.Helper.Enumerations;
 
 namespace LandsWa.Acceptance.Smoke.Tests.Tests
 {
     public class LoginPageTests : BaseTest
     {
-        [Test]
-        public void VerifyThatManagerCanLogin()
+        TeamDashboardPage teamDashboard;
+        LoginPage loginPage;
+        MyDashboardPage myDashboard;
+
+        [TestCase("SophiaAss", "Sophia", "infy4321", User.Manager)]
+        [TestCase("BenAss", "Ben", "infy4321", User.Officer)]
+        public void VerifyThatUserCanLogin(string login, string name, string password, User user)
         {
-            LoginPage.EnterUsername("SophiaAss", Helper.Enumerations.User.Manager)
-                .EnterPassword("infy4321")
+            loginPage = new LoginPage(Driver);
+
+            var dashboard = loginPage.EnterUsername(login, user)
+                .EnterPassword(password)
                 .ClickLoginButton();
+
+            switch (loginPage.userProfile)
+            {
+                case User.Manager:
+                    teamDashboard = (TeamDashboardPage)dashboard;
+                    Assert.IsTrue(teamDashboard.IsPageLoadComplete());
+                    Assert.IsTrue(teamDashboard.IsManagerNameDisplayed(name));
+                    break;
+                case User.Officer:
+                    myDashboard = (MyDashboardPage)dashboard;
+                    Assert.IsTrue(myDashboard.IsPageLoadComplete());
+                    Assert.IsTrue(myDashboard.IsOfficerNameDisplayed(name));
+                    break;
+            }
         }
     }
 }
