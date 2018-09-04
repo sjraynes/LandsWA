753dd9c3d551c6e1986dab81eabbf4590f3f4791
sjraynes <42663185+sjraynes@users.noreply.github.com>
2018-09-04 10:56:12 +0800

    Adding Event path in Application Submission

--- LandsWa.Acceptance.Smoke.Tests/Pages/RequestDetailsMileStone.cs
+++ LandsWa.Acceptance.Smoke.Tests/Pages/RequestDetailsMileStone.cs
@@ -27,6 +27,26 @@ namespace LandsWa.Acceptance.Smoke.Tests.Pages
         protected string dateSigned = "//label[text()='Date Signed']/../../div[2]//input";
         protected string uploadButton = "//button[text()='Upload']";
         protected string uploadField = "//input[@class='MultipleFileUploadWidget---ui-inaccessible']";
+        // Event Request Type Inputs
+        protected string eventFromDate = "//span[text()='From']/../../div[2]//input[contains(@class,'DatePickerWidget')]";
+        protected string eventFromTime = "//span[text()='From']/../../div[2]//input[contains(@class,'TimeWidget')]";
+        protected string eventToDate = "//span[text()='To']/../../div[2]//input[contains(@class,'DatePickerWidget')]";
+        protected string eventToTime = "//span[text()='To']/../../div[2]//input[contains(@class,'TimeWidget')]";
+        protected string eventPurpose = "//textarea";
+        protected string eventLandSize = "//label[text()='Approximate size of the land area required for the event']/../../div[2]/div/input";
+        protected string eventUploadLayoutDocument = "(//input[@class='MultipleFileUploadWidget---ui-inaccessible'])[1]";
+        protected string eventExternalStructuresYes = "//input[@value='Yes']";
+        protected string eventExternalStructuresNo = "//input[@value='No']";
+        protected string eventVisitors = "//*[contains(text(),'Visitor')]/../../td[2]/div/input";
+        protected string eventEmployees = "//*[contains(text(),'Employees')]/../../td[2]/div/input";
+        protected string eventArtists = "//*[contains(text(),'Artists')]/../../td[2]/div/input";
+        protected string eventRevenue = "//*[contains(text(),'Revenue')]/../../td[2]/div/input";
+        protected string eventGrants = "//*[contains(text(),'Grants')]/../../td[2]/div/input";
+        protected string eventExpenditure = "//*[contains(text(),'Expenditure')]/../../td[2]/div/input";
+        protected string eventApplicationDateReceived = "//label[text()='Date Received']/../../div[2]//input[contains(@class,'DatePickerWidget')]";
+        protected string eventUploadApplicantsRequest = "(//input[@class='MultipleFileUploadWidget---ui-inaccessible'])[3]";
+        protected string eventUploadGrantDocument = "(//input[@class='MultipleFileUploadWidget---ui-inaccessible'])[2]";
+
         protected string currentDate = DateTime.Now.ToString("dd/MM/yyyy");
         protected string errorMessage = "//*[contains(text(), 'A value is required')]";
 
@@ -39,6 +59,35 @@ namespace LandsWa.Acceptance.Smoke.Tests.Pages
         public RequestDetailsMileStone SelectGeneralRequestType()
         {
             GetElementByXpath(genrealRequestType).Click();
+            return this;
+        }
+
+        public RequestDetailsMileStone SelectEventRequestType()
+        {
+            GetElementByXpath(eventRequestRequestType).Click();
+            return this;
+        }
+
+        public RequestDetailsMileStone CompleteEventDetails()
+        {
+            GetElementByXpath(eventFromDate).SendKeys("28/11/2020");
+            GetElementByXpath(eventFromTime).SendKeys("10:00 am");
+            GetElementByXpath(eventToDate).SendKeys("30/11/2020");
+            GetElementByXpath(eventToTime).SendKeys("11:35 pm");
+            GetElementByXpath(eventPurpose).SendKeys("Splendour in the Grass");
+            GetElementByXpath(eventLandSize).SendKeys("24596");
+            UploadDocument(_driver.FindElement(By.XPath(eventUploadLayoutDocument)), "20180619 Event Layout or map.docx");
+            GetElementByXpath(eventVisitors).SendKeys("24500");
+            GetElementByXpath(eventEmployees).SendKeys("75");
+            GetElementByXpath(eventArtists).SendKeys("14");
+            GetElementByXpath(eventRevenue).SendKeys("750000");
+            GetElementByXpath(eventGrants).SendKeys("175250");
+            GetElementByXpath(eventExpenditure).SendKeys("458769");
+            GetElementByXpath(eventApplicationDateReceived).SendKeys("03/09/2018");
+            GetElementByXpath(staticPageElement).Click();
+            UploadDocument(_driver.FindElement(By.XPath(eventUploadApplicantsRequest)), "20180611_Applicants_Request_01.docx");
+            UploadDocument(_driver.FindElement(By.XPath(eventUploadGrantDocument)), "20180619 Event Grant or Sponsorship Details.docx");
+
             return this;
         }
 
