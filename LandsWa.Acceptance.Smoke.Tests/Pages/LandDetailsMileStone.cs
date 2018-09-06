882943c9e3b7983addd151a36faea042b057ad2c
sjraynes <42663185+sjraynes@users.noreply.github.com>
2018-09-06 09:46:25 +0800

    Update LandDetailsMileStone.cs
    Add inserting more detail into the Land Details function

--- LandsWa.Acceptance.Smoke.Tests/Pages/LandDetailsMileStone.cs
+++ LandsWa.Acceptance.Smoke.Tests/Pages/LandDetailsMileStone.cs
@@ -19,8 +19,21 @@ namespace LandsWa.Acceptance.Smoke.Tests.Pages
         protected string addButton = "//button[text()='ADD']";
         protected string continueButton = "//button[text()='Continue']";
         protected string selectedLGATableCell = "//tbody/tr/td[9]";
+        protected string LandRecordLotNumber = "//label[text()='Lot Number']/../../div[2]/div/input";
+        protected string LandRecordPlanNumber = "//label[text()='Plan Number']/../../div[2]/div/input";
+        protected string LandRecordPIN = "//label[text()='PIN']/../../div[2]/div/input";
+        protected string LandRecordReserveNumberber = "//label[text()='Reserve Number(if applicable)']/../../div[2]/div/input";
+        protected string LandRecordStreetNumber = "//label[text()='Street Number']/../../div[2]/div/input";
+        protected string LandRecordStreetName = "//label[text()='Street Name']/../../div[2]/div/input";
+        protected string LandRecordSuburb = "//span[text()='Suburb']/../..//input";
+        protected string selectedLandRecordSuburb = "//span[@class='PickerTokenWidget---label']";
+
+        protected string LandRecordComment = "//label[text()='Land Record Comment']/../../div[2]/div/textarea";
+
+        protected string GenInfoCoOrdinates = "//textarea";
 
         protected string LGAName = null;
+        protected string suburb = null;
 
         public LandDetailsMileStone(IWebDriver driver) : base(driver)
         {
@@ -28,17 +41,45 @@ namespace LandsWa.Acceptance.Smoke.Tests.Pages
             GetElementByXpath(staticPageElement).Click();
         }
 
-        public LandDetailsMileStone AddLandRecordForLGA(string LGAName)
+        public LandDetailsMileStone AddLandRecordForLGA(string LGAName, string suburb)
         {
             ClickOnAddLandRecordImage();
+            FillLandRecordEntry(suburb);
             AddLGA(LGAName);
             ClickAddButton();
+            return this;
+        }
+
+        public LandDetailsMileStone AddGeneralInformation()
+        {
+            AddCoOrdinates();
             return this;
         }
 
         public LandDetailsMileStone ClickOnAddLandRecordImage()
         {
             GetElementByXpath(addLandRecordImg).Click();
+            return this;
+        }
+
+        public LandDetailsMileStone FillLandRecordEntry(string fillsuburb)
+        {
+            GetElementByXpath(LandRecordLotNumber).SendKeys("745301");
+            GetElementByXpath(LandRecordPlanNumber).SendKeys("248501");
+            GetElementByXpath(LandRecordPIN).SendKeys("8501");
+            GetElementByXpath(LandRecordReserveNumberber).SendKeys("6371");
+            GetElementByXpath(LandRecordStreetNumber).SendKeys("47");
+            GetElementByXpath(LandRecordStreetName).SendKeys("Lambert");
+
+            var ele = GetElementByXpath(LandRecordSuburb);
+            ele.SendKeys(fillsuburb);
+            Thread.Sleep(1000);
+            ele.SendKeys(Keys.ArrowDown);
+            Thread.Sleep(1000);
+            ele.SendKeys(Keys.Enter);
+            this.suburb = GetElementByXpath(selectedLandRecordSuburb).Text;
+
+            GetElementByXpath(LandRecordComment).SendKeys("Land Record created and inserted during an Application Submission. This Comment was added by an Automation Script written by the LandsWA Test Team.");
             return this;
         }
 
@@ -63,6 +104,12 @@ namespace LandsWa.Acceptance.Smoke.Tests.Pages
         public bool IsLandRecordAdded()
         {
             return GetElementByXpath(selectedLGATableCell).Text == LGAName;
+        }
+
+        public LandDetailsMileStone AddCoOrdinates()
+        {
+            GetElementByXpath(GenInfoCoOrdinates).SendKeys("This is the General Information comment that usually holds the LandRecord Co-Ordinates. It is part of the LandDetails page of the Application Support process. Note that this Case and this comment was added using Automated Test Scripts created by the LandsWA Test Team.");
+            return this;
         }
 
         public ConsultationMileStone ClickContinueButton()
